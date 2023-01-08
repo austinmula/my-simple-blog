1a51fc9b75b0997024fd832d00cbdda34bdcdf76
cymula <amula@cytonn.com>
2023-01-08 16:12:01 +0300

    fixed redirects for create comment

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,13 +2,15 @@ const connection = require("../db/db.config")
 const moment = require('moment')
 
 exports.createComment = (req, res) => {
+    const id = req.body.post_id
     try {
         let query = `INSERT INTO comments (text, user_id, created_at, post_id) VALUES (?, ?, ?, ?); `;
         console.log(moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"))
 
         connection.query(query, [req.body.text, 2, moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"), req.body.post_id], (error, response) => {
             if (!error) {
-                res.json({ msg: "comment added successfuly" })
+                res.redirect(`/post/${id}`)
+                // res.json({ msg: "comment added successfuly" })
                 console.log(response);
             } else {
                 console.log(error)
